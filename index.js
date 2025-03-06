699e56aabe62cc1f99a6ef5bae3a297d3e25567f
calvis onyango <calvisonyango265@gmail.com>
2025-03-06 23:03:28 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+const companyName = "Scuber"
+
+// equalised to chealse
+//  const mostProfitableNeighborhood = "Chelsea"
+
+//  let declare using let
+let mostProfitableNeighborhood = "Chelsea";
+
+const companyCeo ="Susan Smith";
+
+// let companyCeo ="Susan Smith";
